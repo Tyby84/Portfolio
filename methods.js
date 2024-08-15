9e0bc0822b525e030b948bf38e4d4829324ce553
Tyby <tibofenyvesi84@gmail.com>
2024-08-15 18:21:40 +0200

    intro

--- methods.js
+++ methods.js
@@ -2,10 +2,9 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
-    document.getElementById("navbar").style.paddingTop = "10px";
-    document.getElementById("navbar").style.paddingBottom = "10px";
+    document.getElementById("navbar").style.display = "flex";
+    
   } else {
-    document.getElementById("navbar").style.paddingTop = "30px";
-    document.getElementById("navbar").style.paddingBottom = "30px";
+    //document.getElementById("navbar").style.display = "none";
   }
 }
