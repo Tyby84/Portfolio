ee9c64d318defbdea6472a50a08bb9de5ae8e8ca
Tyby <tibofenyvesi84@gmail.com>
2024-08-28 18:27:41 +0200

    responsive2

--- methods.js
+++ methods.js
@@ -2,7 +2,7 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
-    document.getElementById("navbar").style.display = "flex";
+    //document.getElementById("navbar").style.display = "flex";
     
   } else {
     //document.getElementById("navbar").style.display = "none";
